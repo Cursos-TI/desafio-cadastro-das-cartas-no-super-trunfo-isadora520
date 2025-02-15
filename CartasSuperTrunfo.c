7250cd3fc898658a405ade8733a7c280e5444259
isadora520 <isadorasoares5208@gmail.com>
2025-02-15 16:39:32 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,42 @@
 //Teste larissa
 
 int main() {
+
+// Definição das variáveis para cada atributo da cidade 
+    
+    int codigo_cidade = A00
+    char[20] nome = Porto Velho
+    double população = 460.434
+    double área = 32.082
+    double PIB = R$ 20.059.522,00
+    int pontos_turisticos = 70
+
+// Início do código para entrada e saída de dados
+
+    // Usuário pode inserir o código da cidade
+     printf ("Digite aqui o código da cidade utilizando uma letra e um número: \n");
+     scanf ("%d, &codigo_cidade \n);
+    
+
+     //Usuário pode inserir o nome da cidade
+     printf ("Digite aqui o nome da cidade: \n");
+     scanf ("%s, &nome \n);
+     printf ("Nome da cidade: %s \s", nome);
+
+
+
+
+//Resultado com todas as informações da carta
+    
+      printf ("Código da cidade: %d \n",    
+     codigo_cidade)
+
+    
+        
+    
+    
+    
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
