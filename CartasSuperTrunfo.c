6e399f070c53e6a1d6a47d3dec38728e65a448d7
isadora520 <isadorasoares5208@gmail.com>
2025-02-18 15:11:57 -0300

    CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,69 +1,105 @@
 #include <stdio.h>
-
+#include <locale.h>
 
 int main() {
 
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
+    //Início do jogo
+
+    printf ("Bem vindo ao jogo de super trunfo. Vamos começar cadastrando as suas cartas.");
+
 // Definição das variáveis para cada atributo da cidade 
-    
-    int codigo_cidade = A00
-    char[20] nome = Porto Velho
-    double populacao = 460.434
-    double area = 32.082
-    double PIB = R$ 20.059.522,00
-    int pontos_turisticos = 70
+
+char cidade[50], codigo[50], estado[50];
+int populacao, pontos_turisticos;
+float PIB, area;
 
 // Início do código para entrada e saída de dados
 
-    // Usuário pode inserir o código da cidade
-     printf ("Digite aqui o código da cidade utilizando uma letra e um número: \n");
-     scanf ("%d, &codigo_cidade \n);
-    
-
-     //Usuário pode inserir o nome da cidade
-     printf ("Digite aqui o nome da cidade: \n");
-     scanf ("%s, &nome \n);
+// Usuário pode inserir o código da cidade
+printf ("Digite aqui o código da cidade utilizando uma letra e um número:\n");
+scanf ("%s\n", &codigo);
 
 
-    //Usuário pode inserir a população
-     printf ("Digite aqui o número populacional da cidade: \n");
-     scanf ("%e, &populacao \n);
+ //Usuário pode inserir o nome da cidade
+printf ("Digite aqui o nome da cidade:\n");
+scanf ("%s\n", &cidade);
+
+//Usuário pode inserir o estado
+printf ("Digite aqui o estado:\n");
+scanf ("%s\n", &estado);
 
 
-    //Usuário pode inserir a área
-     printf ("Digite aqui qual é a área da cidade: \n");
-     scanf ("%e, &area \n);
+//Usuário pode inserir a população
+printf ("Digite aqui o número populacional da cidade:\n");
+scanf ("%d\n", &populacao);
 
 
-    //Usuário pode inserir o PIB
-      printf ("Digite aqui o PIB da cidade: \n");
-      scanf ("%e, &PIB \n);
+//Usuário pode inserir a área
+printf ("Digite aqui qual é a área da cidade:\n");
+scanf ("%f\n", &area);
 
 
-    //Usuário pode inserir os pontos turísticos
-       printf ("Digite aqui o número de pontos turísticos: \n");
-       scanf (%d, &pontos_turisticos \n);
-         
+//Usuário pode inserir o PIB
+printf ("Digite aqui o PIB da cidade:\n");
+scanf ("%f\n", &PIB);
 
 
-
+//Usuário pode inserir os pontos turísticos
+printf ("Digite aqui o número de pontos turísticos:\n");
+scanf ("%d\n", &pontos_turisticos);
 
 //Resultado com todas as informações da carta
-    
-      printf ("Código da cidade: %d \n",    
-     codigo_cidade);
 
-      printf ("Nome: %s \n", nome);
+printf ("Código da cidade: %s\nCidade: %s\nEstado: %s\nPopulacao: %d\nPontos Turisticos: %d\nPIB: %f\nÁrea: %f", codigo, cidade, estado, populacao, area, PIB, pontos_turisticos);
 
-      printf ("Número populacional: %e \n", populacao);
 
-      printf ("PIB: %e \n", PIB);
 
-      printf ("Número de pontos turísticos: %d \n", pontos_turisticos);
+// Definição das variáveis para cada atributo da cidade 
 
-    
-        
-    
-    
-    
-   return 0;
+char cidade2[50], codigo2[50], estado2[50];
+int populacao2, pontos_turisticos2;
+float PIB2, area2;
+
+// Início do código para entrada e saída de dados
+
+// Usuário pode inserir o código da cidade
+printf ("Digite aqui o código da cidade utilizando uma letra e um número:\n");
+scanf ("%s\n", &codigo2);
+
+
+ //Usuário pode inserir o nome da cidade
+printf ("Digite aqui o nome da cidade:\n");
+scanf ("%s\n", &cidade2);
+
+//Usuário pode inserir o estado
+printf ("Digite aqui o estado:\n");
+scanf ("%s\n", &estado2);
+
+
+//Usuário pode inserir a população
+printf ("Digite aqui o número populacional da cidade:\n");
+scanf ("%d\n", &populacao2);
+
+
+//Usuário pode inserir a área
+printf ("Digite aqui qual é a área da cidade:\n");
+scanf ("%f\n", &area2);
+
+
+//Usuário pode inserir o PIB
+printf ("Digite aqui o PIB da cidade:\n");
+scanf ("%f\n", &PIB2);
+
+
+//Usuário pode inserir os pontos turísticos
+printf ("Digite aqui o número de pontos turísticos:\n");
+scanf ("%d\n", &pontos_turisticos2);
+
+//Resultado com todas as informações da carta
+
+printf ("Código da cidade: %s\nCidade: %s\nEstado: %s\nPopulacao: %d\nPontos Turisticos: %d\nPIB: %f\nÁrea: %f", codigo2, cidade2, estado2, populacao2, area2, PIB2, pontos_turisticos2);
+
+return 0;
 }
